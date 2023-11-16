049e80366eed4dd3c6754a5b8d857ef0555ab067
Chosen808 <99940594+Chosen808@users.noreply.github.com>
2023-11-16 17:43:06 -0400

    Commit 161123 1740 - Makefile created

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -2,11 +2,30 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
 
 int main()
 {
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
     std::cout << "Calculator COnsole Application" << std::endl << std::endl;
     std::cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << std::endl;
+
+    Calculator c;
+
+    while (true)
+    {
+        std::cin >> x >> oper >> y;
+
+        result = c.Calculate(x, oper, y);
+
+        std::cout << "Result " << "of " << x << oper << y << " is: " << result << std::endl;
+
+        std::cout << "Please enter another one: " << std::endl;
+    }
 
 
     return 0;
