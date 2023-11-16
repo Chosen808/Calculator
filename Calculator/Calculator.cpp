791ffefe872593beaf75e8ba13ec50d73da62314
Chosen808 <99940594+Chosen808@users.noreply.github.com>
2023-11-15 20:00:06 -0400

    Add project files.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,14 @@
+// Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Calculator COnsole Application" << std::endl << std::endl;
+    std::cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << std::endl;
+    std::cout << "TEST" << std::endl;
+
+
+    return 0;
+}
